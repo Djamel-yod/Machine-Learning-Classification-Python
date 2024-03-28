9359a312e608a0233031ffc19194bee1634ef520
Djamel-yod <isma1yoda@gmail.com>
2024-03-28 21:02:30 +0100

    Prédiction Churn

--- app.py
+++ app.py
@@ -24,7 +24,7 @@ model = pickle.load(open("model.pkl", "rb"))
 # Titre de l'application
 st.title("Prédiction de la résiliation de contrat des abonnées d'une société de télécommunication")
 st.subheader('Auteur: Ismael YODA')
-st.write("Cette application est destinée a prédire si un client d'une société de télécommunication a une forte probabilité de résilier son contract ou non en utilisant un algorithme de Machine Learning qui offre une grande précision dans la prédiction.")
+st.write("Cette application est destinée a prédire si un client d'une société de télécommunication a une forte probabilité de résilier son contract en utilisant un algorithme de Machine Learning qui offre une grande précision dans la prédiction.")
 st.write("Entrez les informations du client ou de la cliente pour prédire s'il(elle) a une grande probabilité de résilier son contrat")
 
 col1, col2 = st.columns(2)
